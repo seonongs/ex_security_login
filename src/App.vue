8312f9260160f9da970ef0ff1a8f97205c83f23d
seonongs <seonongs@gmail.com>
2022-08-01 03:34:59 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div>
+
+  </div>
+
+  <RouterView />
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input type="text" />
+    <input type="text" />
+    <button>go</button>
+  </div>
+</template>
+
+<style>
+</style>
